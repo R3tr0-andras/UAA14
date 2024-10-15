92bd4269d762c42702591fcd55e19d167232ce3e
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-10-15 11:31:05 +0200

    interro

---
+++ interro/6ttiAndrasInterroN2POO/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _6ttiAndrasInterroN2POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Set up
+            TraficLight one = new TraficLight("007", false, '0');
+            TraficLight two = new TraficLight("1001", false, '0');
+            TraficLight[] traficLights = new TraficLight[2];
+            traficLights[0] = one;
+            traficLights[1] = two;
+            
+
+            //Porgramme principale
+            Console.WriteLine("Etat et couleurs des feux : ");
+            Console.WriteLine("---------------------------------");
+            for (int i = 0;i < 2; i++) // fait passer du rouge au vert
+            {
+                traficLights[0].ChangeColor();
+                traficLights[1].ChangeColor();
+            }
+            Console.WriteLine($"Le feu de signalisation 1001 est vert et éteint");
+            Console.WriteLine($"Le feu de signalisation 007 est vert et éteint");
+            Console.WriteLine("\n");
+            Console.WriteLine("Faire passer le 007 à l'orange :");
+            Console.WriteLine("---------------------------------");
+            traficLights[0].ChangeColor(); // passe du rouge au oarange
+            traficLights[0].TurnOnOrOff(); // Allume le feu
+            Console.WriteLine("Le feu de signalisation 007 est Orange et allumé");
+            Console.WriteLine("\n");
+            Console.WriteLine("Feu clignotant");
+            Console.WriteLine("---------------------------------");
+            int nbAlternance = 6 ; // on le fait clignoter 6fois dans l'énoncé
+            for (int i = 0; i < nbAlternance; i++)
+            {
+                traficLights[0].WinkLight();
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Changement d'état : ");
+            Console.WriteLine("---------------------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                traficLights[1].ChangeColor();
+            }
+            Console.WriteLine("Le feu de signalisation 1001 est rouge");
+            Console.WriteLine("Le feu de signalisation 1001 est orange");
+            Console.WriteLine("Le feu de signalisation 1001 est vert");
+            Console.WriteLine("Le feu de signalisation 1001 est rouge");
+            Console.WriteLine("Le feu de signalisation 1001 est orange");
+        }
+    }
+}
---
+++ interro/6ttiAndrasInterroN2POO/TraficLight.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6ttiAndrasInterroN2POO
+{
+    class TraficLight
+    {
+        private string _traficLightId;
+        private bool _isOn;
+        private int _colorNow;
+
+        public TraficLight(string traficLightId, bool isOn, int colorNow)
+        {
+            _traficLightId = traficLightId;
+            _isOn = isOn;
+            _colorNow = colorNow;
+        }
+
+        public string TraficLightId
+        {
+            get { return _traficLightId; }
+
+        }
+        public int ChangeColor()
+        {
+            if (_isOn)
+            {
+                if (_colorNow == 0) //rouge
+                {
+                    _colorNow = 1;
+                }
+                else if (_colorNow == 1) //orange
+                {
+                    _colorNow = 2;
+                }
+                else if (_colorNow == 2) //vert
+                {
+                    _colorNow = 0;
+                }
+                else //exception
+                {
+                    Console.WriteLine("en dehors des paramètres");
+                }
+            }
+            else
+            {
+                _colorNow = _colorNow;
+            }
+
+            return _colorNow;
+        }
+
+        public bool TurnOnOrOff()
+        {
+            if (_isOn)
+            {
+                _isOn = true;
+            }
+            else
+            {
+                _isOn = false;
+            }
+            return _isOn;
+        }
+
+        public void WinkLight()
+        {
+            if (_isOn)
+            {
+                _isOn = false;
+                Console.WriteLine($"Le feu de signalisation {_traficLightId} est éteint.");
+            }
+            else
+            {
+                _isOn = true;
+                Console.WriteLine($"Le feu de signalisation {_traficLightId} est allumé.");
+            }
+        }
+
+        
+        
+    }
+}
