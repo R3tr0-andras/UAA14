90870dd7f03b8668a45817ffb71aeeaab73fd942
Andras-corda <220672@site.asty-moulin.be>
2025-01-14 10:45:16 +0100

    -

---
+++ Act9/6tti_andras_ClassesLiees_simplifie/Bibliotheque.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6tti_andras_ClassesLiees_simplifie
+{
+    internal class Bibliotheque
+    {
+        private List<Livre> _livres;
+
+        public List<Livre> Livres { get { return _livres; } set { _livres = value; } }
+
+        public Bibliotheque()
+        {
+            _livres = new List<Livre>();
+        }
+
+        public void ajoute(Livre livre)
+        {
+            _livres.Add(livre);
+        }
+
+        public void supprime()
+        {
+           _livres.RemoveAll((livre) =>  livre.Etat == 0);
+        }
+
+        public string inventaire()
+        {
+            string temp = "";
+            foreach (Livre livre1 in _livres)
+            {
+                temp = livre1.Titre + ", ";
+            }
+            return $"{temp}";
+        }
+    }
+}
---
+++ Act9/6tti_andras_ClassesLiees_simplifie/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _6tti_andras_ClassesLiees_simplifie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Biblio
+            Bibliotheque bibliotheque = new Bibliotheque();
+
+            // Livres
+            Livre a = new Livre("a", "a", 5);
+            Console.WriteLine(a.description());
+            Livre b = new Livre("b", "b", 4);
+            Console.WriteLine(b.description());
+            Livre c = new Livre("c", "c", 3);
+            Console.WriteLine(c.description());
+            Livre d = new Livre("d", "d", 2);
+            Console.WriteLine(d.description());
+            Livre e = new Livre("e", "e", 1);
+            Console.WriteLine(e.description());
+            Livre f = new Livre("f", "f", 0);
+            Console.WriteLine(f.description());
+
+            //degarde
+            a.degrade(4);
+            Console.WriteLine(a.description());
+
+            // Ajout
+            bibliotheque.ajoute(a);
+            bibliotheque.ajoute(b);
+            bibliotheque.ajoute(c);
+            bibliotheque.ajoute(d);
+            bibliotheque.ajoute(e);
+            bibliotheque.ajoute(f);
+
+            // inventaire
+            Console.WriteLine(bibliotheque.inventaire());
+
+            // Supprime
+            bibliotheque.supprime();
+            Console.WriteLine(bibliotheque.inventaire());
+        }
+    }
+}
