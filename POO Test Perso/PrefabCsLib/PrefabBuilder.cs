cf613cc7913ae5be872e9a22489b6aa982e72364
Andras-corda <220672@site.asty-moulin.be>
2025-04-25 20:23:28 +0200

    -

---
+++ POO Test Perso/PrefabCsLib/PrefabBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefabCsLib
+{
+    public class PrefabBuilder<T> where T : IPrefab
+    {
+        private T _instance;
+
+        public PrefabBuilder(T emptyInstance)
+        {
+            _instance = emptyInstance;
+        }
+
+        public T Instance => _instance;
+
+        public void ApplyChanges(Action<T> configure)
+        {
+            configure(_instance);
+        }
+
+        public void SaveAsPrefab()
+        {
+            PrefabManager.RegisterPrefab(_instance);
+        }
+    }
+}
---
+++ POO Test Perso/PrefabCsLib/PrefabManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefabCsLib
+{
+    public static class PrefabManager
+    {
+    
+    private static readonly Dictionary<string, IPrefab> _prefabs = new();
+
+    public static void RegisterPrefab(IPrefab prefab)
+    {
+        _prefabs[prefab.PrefabId] = prefab;
+    }
+
+    public static IPrefab Instantiate(string prefabId)
+    {
+        if (_prefabs.TryGetValue(prefabId, out var prefab))
+            return prefab.Clone();
+
+        throw new KeyNotFoundException($"Prefab with ID '{prefabId}' not found.");
+    }
+}
+}
